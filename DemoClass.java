94593e8f3f583de1b362ef11dda7546489a44fb4
kkart1 <127393872+kkart1@users.noreply.github.com>
2024-03-19 13:20:01 +0530

    Add files via upload
    Adding Source File

---
+++ DemoClass.java
@@ -0,0 +1,14 @@
+package com.demo.pack;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		
+		System.out.println(a);
+
+	}
+
+}
